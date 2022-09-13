358820c8869bb03171523df1f44793820855b1bb
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2022-09-13 20:29:56 +0800

    1

--- xc原版客户端备份+追踪更新.js
+++ xc原版客户端备份+追踪更新.js
@@ -38,9 +38,8 @@ var allowSend = true;
 var showHead = true;
 var autoLogin = false;
 var showVoiceBtn = true;
-var showReviveBtn = true;
 var showVoiceMsg = true;
-var received_bye = false;//鱼鱼的新神秘功能，我猜是用来搞卢本伟的
+var received_bye = false;
 var onLineNum = 0;
 var defaultHeadPic = 'https://xq.kzw.ink/imgs/tx.png'
 var allowOld = true;
@@ -113,7 +112,7 @@ md.use(remarkableKatex);
 function verifyLink(link) {
 	var linkHref = Remarkable.utils.escapeHtml(Remarkable.utils.replaceEntities(link.href));
 	if (linkHref !== link.innerHTML) {
-		return confirm('[NCX] 请确认链接是否正确：' + linkHref);
+		return confirm('警告，请确认这是你想去的地方:' + linkHref);
 	}
 
 	return true;
@@ -125,27 +124,18 @@ var verifyNickname = function (nick) {
 	//return true
 }
 
-//主页
-var homeText = "# NewCrackedX\n##### \n-----\n"+
-"当前NCX版本：beta0.1(alpha0.12)\n"+
-"对应XC版本：22/8/16\n"+
-"**更新日志：**\n"+
-"成功获取XC全部网页文件并在本地HTTP服务器正常访问；\nWS地址修改，可以正常连接XC；\n验证码地址替换，可以正常输入验证码；指纹替换；\n新增封杀免疫功能，且HTML消息会额外在控制台输出；\n主页更改为NCX主页；\n字体改为本地资源\n"+
-"新增指纹种子功能，可以修改指纹种子，可以自动随机生成指纹种子；\n主页修改；\n新增免疫shout功能；\n新增真私聊功能；ajax改为本地资源；\n安全私聊功能修改；\n更新为新版昵称显示系统（支持昵称颜色、管理员星星）"+
-"\n"+
-"--- \n"+
-"共有聊天室：[?xq102210](/?xq102210) \n"+
-"机器人聊天室：[?bot](/?bot) \n"+
-"挂机池：[?75tz2vv8](/?75tz2vv8) \n"+
-"随机聊天室： ?" +Math.random().toString(36).slice(2, 10)+ "\n"+
-"--- \n"+
-"由ee编写的使用手册：https://paperee.tk/XChat.html  \n"+
-"开源网址：https://gitee.com/liguiyu102210/xchat  \n"+
-"hackchat开源网址：https://github.com/hack-chat/  \n"+
-"--- \n"+
-"鸣谢用户：[@ee](https://paperee.tk)，[@Mr_Zhang](https://mrzhang365.github.io/)\n"+
-"友情链接：[XChat库](https://github.com/MrZhang365/XChat-Lib-for-Python) [Homer-信鸽](https://gitee.com/BirdingLight/homer/releases/) [TChat-原生hc中文聊天室](https://chat.thz.cool)\n"+
-"~~2022 XQ Team.~~\n2022 YC USERS.";
+var homeText = "# XChat\n##### \n-----\n"+
+"欢迎来到XChat，这是一个极简、小型的网页在线聊天工具，是HC的升级版\n"+
+"共有聊天室：**[?xq102210](/?xq102210)** \n"+
+"如果您单纯的想玩机器人，应避免在共有聊天室刷屏，您可以前往机器人聊天室：**[?bot](/?bot)** \n"+
+"或者您可以访问这个为您准备的聊天室（只有您自己）：  ?" +Math.random().toString(36).substr(2, 8)+
+"\n如果您还不了解这个聊天室，可以访问这个[由@ee编写的使用手册](https://paperee.tk/XChat.html)\n"+
+"\n-----\n该项目是开源的，欢迎前往点Star，开源网址：https://gitee.com/XChatFish/xchat  \n"+
+"XChat改编自hackchat开源项目，hackchat开源网址：https://github.com/hack-chat/  \n"+
+"**如需商用、二次运营本开源项目，请注明本站！** \n-----\n"+
+"鸣谢用户：\n   [@ee](https://paperee.tk), [@Mr_Zhang](https://mrzhang365.github.io/)\n感谢以上用户对本站精神和技术的支持 \n **免责声明：上述用户任何行动、言论与线圈无关且不代表XQTeam立场。** \n-----\n"+
+"友情链接：\n [简化XChat bot开发的python库(强烈推荐)](https://github.com/MrZhang365/XChat-Lib-for-Python) \n [由XChat代理，支持联机的JSPVZ](http://xq.kzw.ink:5901)  \n-----\n"+
+"2022 XQ Team.";
 
 function $$(query) {
 	return document.querySelector(query);
@@ -277,7 +267,7 @@ if (localStorageGet('allow-html') === 'true') {
 allow_HTML_check.onchange = function (e) {
 	var enabled = !!e.target.checked;
 	if ( !!e.target.checked ) {
-		pushMessage({ nick: '!', text: '# 安全提醒：\n允许显示HTML信息可能会导致一些安全风险（例如==密码被盗取==）。\n非必要不允许显示HTML信息！'});
+		pushMessage({ nick: '!', text: '# 安全提醒：\n允许显示HTML信息可能会导致一些安全风险（例如==密码被盗取==）。\n请不要一直允许显示HTML信息！'});
 	}
 	localStorageSet('allow-html', enabled);
 	allowHTML = enabled;
@@ -327,8 +317,7 @@ function notify(args) {
 		}
 	}
 }
-
-var cookie = localStorageGet('cookie')//更新鱼鱼的cookie功能
+var cookie = localStorageGet('cookie')
 function join(channel) {
 	if (location.search.indexOf('?TEXT_') == 0) {
 		$("#voice").hide();
@@ -346,25 +335,25 @@ function join(channel) {
 	}
 	*/
 	// for local installs
-	//var protocol = location.protocol === 'https:' ? 'wss:' : 'ws:'
+	var protocol = location.protocol === 'https:' ? 'wss:' : 'ws:'
 	// if you changed the port during the server config, change 'wsPath'
 	// to the new port (example: ':8080')
 	// if you are reverse proxying, change 'wsPath' to the new location
 	// (example: '/chat-ws')
-	//ar wsPath = location.protocol === 'https:' ? '/ws' : ':6060';
-	ws = new WebSocket('wss://xq.kzw.ink/ws');
-	//if (location.protocol != 'https:') {
-	//	if (confirm("该通道未加密，部分功能将缺失，是否跳转到加密通道？")) {
-	//		location.href='https://xq.kzw.ink'
-	//		return false
-	//	}
-	//}
+	var wsPath = location.protocol === 'https:' ? '/ws' : ':6060';
+	ws = new WebSocket(protocol + '//' + document.domain + wsPath);
+	if (location.protocol != 'https:') {
+		if (confirm("该通道未加密，部分功能将缺失，是否跳转到加密通道？")) {
+			location.href='https://xq.kzw.ink'
+			return false
+		}
+	}
 	var wasConnected = false;
 
 	ws.onopen = async function () {
 		
 		var shouldConnect = true;
-		var payload = {//更新鱼鱼的新加入代码
+		var payload = {
 			cmd:'join',
 			channel:channel,
 			client_key:'XChatYYDS_'
@@ -384,7 +373,7 @@ function join(channel) {
 						if (confirm(`是否使用这个用户信息登录：\n${myNick}`)){
 							payload.nick = myNick
 							payload.cookie = cookie
-						} else {
+						}else{
 							var newNick = prompt('昵称:', '');
 							if (newNick !== null) {
 								myNick = newNick;
@@ -394,7 +383,7 @@ function join(channel) {
 								shouldConnect = false;
 							}
 						}
-					} else {
+					}else{
 						var newNick = prompt('昵称:', '');
 						if (newNick !== null) {
 							myNick = newNick;
@@ -404,22 +393,13 @@ function join(channel) {
 							shouldConnect = false;
 						}
 					}
+					
 				}
 			}
 		}
-		/*
-		if (localStorageGet('murmurseed')) {
-			murmurseed = localStorageGet('murmurseed')
+		if (!(/^[a-zA-Z0-9_]{1,20}$/.test(myNick.split('#')[0])) && connectHC) {
+			alert('要连接HC，需要使用英文昵称，并且昵称需要少于20位。您可以继续使用，但HC服务器不会连接。')
 		}
-		*/
-		murmurseed = prompt('指纹种子：（留空随机）',localStorageGet('murmurseed')) || Math.random().toString(36).slice(2, 10)
-		/*
-		if (murmurseed === '') {
-			murmurseed = Math.random().toString(36).slice(2, 10)
-		}
-		*/
-		console.log(`[NCX] murmurseed:${murmurseed}`)
-
 		if (!(/^[a-zA-Z0-9_]{1,20}$/.test(myNick.split('#')[0])) && connectHC) {
 			alert('要连接HC，需要使用英文昵称，并且昵称需要少于20位。您可以继续使用，但HC服务器不会连接。')
 		}
@@ -430,15 +410,14 @@ function join(channel) {
 		await getMurmur();
 		if (myNick && shouldConnect) {
 			pushMessage({ nick: '*', text: "正在加入聊天室，这可能需要一些时间，请稍后。。。" });
-			localStorageSet('my-nick', myNick);
-			localStorageSet('murmurseed',murmurseed)
-			console.log(`[NCX] murmur:${murmur}`)
+			localStorageSet('my-nick', myNick.split('#')[0]);
+			console.log(murmur)
 			payload.murmur = murmur
 			send(payload);
 		}
 		var h5_mynick = document.querySelector("#mynick")
 		h5_mynick.innerHTML = '当前昵称：'+myNick.split('#')[0]
-		console.log(`[NCX] allowOld:${allowOld}`)
+		console.log(allowOld)
 		wasConnected = true;
 	}
 
@@ -579,7 +558,7 @@ var COMMANDS = {
 		localStorageSet('auto-login', 'false')
 		localStorageSet("cookie", '')
 		pushMessage({nick:'*',text:'已清除您的登录信息'})
-	},//更新鱼鱼的新cookie功能
+	},
 	chat: function (args) {
 		console.log(args)
 		if (ignoredUsers.indexOf(args.nick) >= 0) {
@@ -592,15 +571,13 @@ var COMMANDS = {
 		if (allowHTML){
 			pushHTML(args)
 		}else{
-			pushMessage({nick:'!',text:`[NCX] ${args.nick} 发送了一条HTML信息，但当前设置禁止显示。已在控制台记录HTML信息。`})
+			pushMessage({nick:'!',text:`${args.nick} 发送了一条HTML信息，但由于你尚未允许显示HTML信息，所以未能显示。\n您可以在侧边栏中勾选“允许显示HTML信息”来开启这个功能。`})
 		}
-		console.log(args)
 	},
 	kill: function (args) {
 		console.log(mynick)
 		if(args.nick==myNick.split('#')[0]) {
-			localStorageSet("killed", 0)
-			pushMessage({ nick: '*', text: "[NCX] 已免疫封杀"});
+			localStorageSet("killed", 1)
 		}
 		pushMessage({ nick: '*', text: "已==封杀== "+args.nick});
 	},
@@ -616,7 +593,7 @@ var COMMANDS = {
 		pushMessage(args);
 	},
 	shout: function (args) {
-		pushMessage({ nick: '*', text:'[NCX] 已免疫shout，shout内容：'+args.text})
+		alert(args.text)
 	},
 
 	warn: function (args) {
@@ -647,7 +624,8 @@ var COMMANDS = {
 		}
 		if (args.cookie){
 			localStorageSet('cookie',args.cookie)
-		}//鱼鱼的cookie功能
+		}
+		
 		
 	},
 
@@ -808,31 +786,25 @@ var COMMANDS = {
 		}, 500);
 	},
 	bye: function(args) {
-		/*
 		received_bye = true
 		md.inline.ruler.enable([ 'katex' ]);
                 md.block.ruler.enable([ 'katex' ]);
 		allowImages = true
-		*/
-		pushMessage({ nick: '*', text: "[NCX] 已免疫bye"});
 
-	},//鱼鱼的新神秘功能
+	},
 	onpass: function (args) {
 		if(args.ispass == 'true') {
 			pass = true
 
-			//免疫语音限制
 			if (location.search.indexOf('?VOICE_') == 0) {
-				pushMessage({ nick: '*', text: '[NCX] 已免疫语音房限制，可以用/me来发言。'})
-				//$("#chatform").hide()
-				//$("#chatinput").hide()
+				$("#chatform").hide()
+				$("#chatinput").hide()
 				$("#show-voice-msg-p").hide()
 				$("#show-voice-btn-p").hide()
 				showVoiceBtn = true
 				showVoiceMsg = true
-				//$("#voice").css('width', '100%').css('height', '3rem');
+				$("#voice").css('width', '100%').css('height', '3rem');
 			}
-			
 			
 			if(allowOld) {
 				console.log('run1', localStorageGet('oldnum'))
@@ -863,8 +835,6 @@ var COMMANDS = {
 }
 
 function pushMessage(args) {
-	console.log('[NCX] 收到消息记录')
-	console.log(args)
 	// Message container
 	var messageEl = document.createElement('div');
 	
@@ -877,9 +847,7 @@ function pushMessage(args) {
 
 	messageEl.classList.add('message');
 
-	if (verifyNickname(myNick) && args.nick == myNick) {
-		messageEl.classList.add('me');
-	} else if (args.nick == '!') {
+	if (args.nick == '!') {
 		messageEl.classList.add('warn');
 	} else if (args.nick == '*') {
 		messageEl.classList.add('info');
@@ -897,7 +865,6 @@ function pushMessage(args) {
 	nickSpanEl.classList.add('nick');
 	messageEl.appendChild(nickSpanEl);
 
-	//加上XC最新抄袭的新用户昵称系统（即和hc一样的管理员名字前面有星星）
 	if (args.trip) {
 	    var tripEl = document.createElement('span');
 	    if (args.mod || args.admin){
@@ -1035,8 +1002,6 @@ function pushMessage(args) {
 		}
 		nickSpanEl.appendChild(nickLinkEl);
 	}
-	
-	//加上XC新抄袭的颜色功能
 	if (args.color && /(^[0-9A-F]{6}$)|(^[0-9A-F]{3}$)/i.test(args.color)){
 		nickLinkEl.setAttribute('style', 'color:#' + args.color + ' !important');
 	}
@@ -1049,9 +1014,9 @@ function pushMessage(args) {
 		if (args.text.indexOf('USERSENDVOICE_')>=0 && showVoiceMsg) {
 			html = `<audio controls><source src="https://xq.kzw.ink/oss/${args.text.replace('USERSENDVOICE_', '').replace('static/', '')}" type="audio/mpeg"></audio>`
 		} else {
-			html = md.render(args.text.replace(/files/g, 'https://xq.kzw.ink/files'))
+			html = md.render(args.text)
 		}
-		textEl.innerHTML = html
+		textEl.innerHTML = html;
 	}else {
 		var html = ''
 		args.text.forEach(v=> {
@@ -1103,7 +1068,6 @@ function pushMessage(args) {
 	unread += 1;
 	updateTitle();
 }
-
 function pushHTML(args) {
 	// Message container
 	var messageEl = document.createElement('div');
@@ -1127,7 +1091,6 @@ function pushHTML(args) {
 	nickSpanEl.classList.add('nick');
 	messageEl.appendChild(nickSpanEl);
 
-	//加上XC最新抄袭的新用户昵称系统（即和hc一样的管理员名字前面有星星）
 	if (args.trip) {
 	    var tripEl = document.createElement('span');
 	    if (args.mod || args.admin){
@@ -1154,11 +1117,6 @@ function pushHTML(args) {
 	textEl.innerHTML = args.text;
 
 	messageEl.appendChild(textEl);
-	
-	//XC原版没加上的HTML消息的颜色功能
-	if (args.color && /(^[0-9A-F]{6}$)|(^[0-9A-F]{3}$)/i.test(args.color)){
-		nickLinkEl.setAttribute('style', 'color:#' + args.color + ' !important');
-	}
 
 	// Scroll to bottom
 	var atBottom = isAtBottom();
@@ -1185,7 +1143,6 @@ function insertAtCursor(text) {
 
 function send(data) {
 	if (ws && ws.readyState == ws.OPEN) {
-		console.log('[NCX] 发送消息记录')
 		console.log(data)
 		if(addOld) {
 			data.show = 1;
@@ -1202,37 +1159,18 @@ function send(data) {
 				//页面命令
 				if (data.text) {
 					if (data.text.startsWith('/')) {
-						//data.text = data.text.substr(1);
-						var [cmd,arg1,arg2] = data.text.substr(1).split(' ')
+						data.text = data.text.substr(1);
+						var [cmd,args] = data.text.split(' ')
 
 						//设置共享视频地址
 						if (cmd == 'video') {
-							console.log(arg1)
-							ws.send(JSON.stringify({cmd:'set_video', url: arg1}));
+							console.log(args)
+							ws.send(JSON.stringify({cmd:'set_video', url: args}));
 							return false;
 						}
 
-						//invite自定义邀请，NCX特殊命令
-						if (cmd == 'invite' || cmd == 'in') {
-							if (!arg2) {
-								arg2 = Math.random().toString(36).slice(2, 10)
-							}
-							ws.send(JSON.stringify({cmd:'invite', nick: arg1.replace(/^@/,''), to: arg2}));
-							console.log(`[NCX] invite邀请 邀请对象：${arg1} 邀请频道：${arg2}`)
-							return true;
-						}
-
-						//invite真私聊，NCX特殊命令
-						if (cmd == 'i' || cmd == 'sw') {
-							let to = ` 实际上是要私信你：${arg2}`
-							ws.send(JSON.stringify({cmd:'invite', nick: arg1.replace(/^@/,''), to: to}));
-							console.log(`[NCX] invite私聊 私聊对象：${arg1} 私聊文本：${arg2}`)
-							return true;
-						}
-
 						//完成后确认没有命令则添加斜杠
-						//data.text = '/'+data.text;
-						//改掉鱼鱼的不太优雅的逻辑（想当年在这里加上斜杠还是我告诉他的，本来当时出现了普通命令要两个斜杠才能用的情况）
+						data.text = '/'+data.text;
 					}
 					
 				}
@@ -1255,13 +1193,114 @@ function send(data) {
 					}
 					*/
 				}
+
+				/* if(data.text == 'help') {
+					// 该帮助文档为原创。
+					let help_docs = `
+# XChat帮助文件（版本：2.00）
+###### 更新日期：北京时间 2022-6-4 10:00
+## 前言
+XChat，是一款由Hack.Chat改变的聊天平台，由线圈团队编写。它有着艰辛的发展历程。为推广XChat，Mr_Zhang特意编写了本帮助文件，希望诸位用户能细细阅读。
+###### 小提示：某些像猪一样愚蠢的人只会胡乱贬低XChat，请不要相信他们。
+## 基本教程
+### 识别码
+#### 介绍
+识别码是一个六位的“乱码”，位于头像左侧，由你的密码生成，可以有效让其他用户识别是否是你本人。
+#### 使用方法
+1. 刷新网页；
+2. 在昵称输入框中按照\`昵称#密码\`的格式填写，如\`Test#123456\`。
+3. 点击确定或者按下回车，进入XChat。
+4. 在这个例子中，\`123456\`就是密码，\`QTCCbB\`是其对应的识别码。
+###### 小提示：一个密码只对应一个识别码。请确保你的密码足够特殊，让别人猜不到你的密码，不能冒充你。除了站长，其他人的识别码都是六位的，站长的识别码是\`Admin\`
+### 机器人（bot）
+#### 介绍
+机器人，简称bot，是由其他开发者开发的，可以服务于大家，也可以活跃聊天气氛。机器人开发者可以通过机器人后台控制机器人发送信息。
+#### 机器人列表
+截至北京时间2022-6-4清晨，机器人有：\`eebot\`、\`SuMx_bot\`、\`Zhang系列Bot\`（昵称以\`Zhang\`开头）、\`dotbot\`、\`zzBot\`（常用昵称为\`zzChumo\`；有时是真人，有时是机器人）
+###### 小提示：开发机器人需要向XChat管理员申请token来让机器人跳过验证码。
+### 发送图片
+#### 介绍
+~~这个就不多介绍了。。。~~
+#### 使用方法
+1. 把头像图片上传到图片托管网站（图床）。
+2. 复制图片托管网站提供的图片URL。
+3. 回到XChat，按照\`![](图片URL)\`的格式发送信息。
+### 设置头像
+#### 介绍
+头像可以让别人更快速地产生对你的第一印象，选择一个好看的头像可以在一定程度上改善别人对你的印象。
+#### 使用方法
+1. 把头像图片上传到图片托管网站（图床）。
+2. 复制图片托管网站提供的图片URL。
+3. 回到XChat，在侧边栏里点击\`设置头像\`按钮，在弹出来的输入框中粘贴图片URL，并点击确定或按下回车。
+###### 小提示：请勿盗用他人头像。
+### 设置背景
+#### 介绍
+~~这个不用多说了吧。。。~~
+#### 使用方法
+~~（和设置头像方法差不多，这里不再废话了）~~
+### 一起看视频
+#### 介绍
+一起看视频，顾名思义，即在聊天室内观看视频，而且可以保证所有人的进度和视频播放者进度相同。属于XChat的特色功能。
+#### 使用方法
+1. 在侧边栏里点击\`一起看视频！\`按钮，即可观看视频。
+###### 小提示：视频可能会失效，所以不是任何时候都能看视频。如需设置视频，可以联系XChat管理员。
+### 连接到HC服务器
+#### 介绍
+该功能可以让你同时加入HackChat聊天室。是XChat的特色功能。
+#### 使用方法
+1. 在侧边栏里勾选\`连接HC服务器\`。
+2. 重新进入XChat。
+3. 把信息发送到HackChat，只需要给你要发送的信息加上前缀\`hc-\`即可。
+#### 备注
+1. 如果你在XChat里的\`?xq102210\`，那么在HackChat的\`?your-channel\`里，会有一名叫做\`XC_你的昵称\`的用户。
+2. 当HackChat用户发信息的时候，你会在XChat看到以\`HC_\`开头的用户，那是HackChat内部的用户。
+###### 小提示：除了XChat内的\`?xq102210\`聊天室，其他聊天室都会镜像连接到HackChat。例如在XChat内的\`?test\`，相应的，你也会加入HackChat的\`?test\`聊天室。
+### 发送语音
+#### 介绍
+该功能可以让你在聊天室内发送语音信息，属于XChat特色功能。
+#### 使用方法
+1. 点击聊天框左侧的\`发送语音\`按钮（可能会弹出权限申请界面，请同意权限申请）。
+2. 要结束录音并发送，可以点击\`结束录音\`按钮。
+###### 小提示：录音最长时间为60秒，超时后会自动结束录音并发送。XChat提供文字聊天室和语音聊天室，即只能发送文字的聊天室和只能发送语音的聊天室。以\`TEXT_\`开头的是文字聊天室，以\`VOICE_\`开头的是语音聊天室（区分大小写）。
+### 创建个人聊天室
+#### 介绍
+~~话不多说，直接进入正题。。。~~
+#### 使用方法
+1. 把网址\`https://xq.kzw.ink/?xq102210\`中的半角问号后更改为你要创建的聊天室的名称，即可创建。例如\`https://xq.kzw.ink/?test\`
+### 私聊
+#### 介绍
+偷偷地给某人发送信息，只有你和对方能看到。
+#### 使用方法
+1. 在聊天室按照\`/w 对方的昵称 要发送的信息\`的格式发送信息即可发送私聊。例如\`/w 小明 你好！\`就是把内容为\`你好\`的信息发送给用户\`小明\`。
+###### 小提示: 当有人向你发送私聊时, 你只需要发送\`/r 要发送的信息\`即可快速回复。
+## 禁止的行为
+1. 乱刷屏（包括发长信息之前没有提醒）
+2. 传播色情内容
+3. 随意攻击某人
+4. 非网络原因进进出出
+5. 所有违反中华人民共和国的内容
+6. 诋毁线圈团队以及XChat（不包括提出问题和建议）
+7. 发送引起公愤的内容
+8. 使用令人反感的昵称
+9. 发送令人反感的内容
+10. 过度使用机器人
+
+==如果违反以上规定，可能会受到踢出聊天室/禁言/封禁处罚==
+
+## 结束语
+我们希望此帮助对您有帮助，如有疑问，请联系XChat用户Mr_Zhang。
+感谢您的阅读！祝您聊天愉快！`;
+
+					pushMessage({nick:'*', text:help_docs})
+					return false;
+				}  */
 				
 				ws.send(JSON.stringify(data));
 			} else {
 				pushMessage({nick:'*', text:'当前昵称已被预留，请更换昵称'})
 			}
 		} else {
-			alert('你已被管理员封杀，无法执行此操作。\n[NCX] 清除网站数据即可解除封杀。我们致力于让NCX用户能够免疫封杀。')
+			alert('你已被管理员封杀，无法执行此操作。')
 		}
 		
 	}
@@ -1535,6 +1574,7 @@ $$('#joined-left').onchange = function (e) {
 }
 
 $$('#parse-latex').onchange = function (e) {
+	if (!received_bye){
 	var enabled = !!e.target.checked;
 	localStorageSet('parse-latex', enabled);
 	if (enabled) {
@@ -1543,6 +1583,7 @@ $$('#parse-latex').onchange = function (e) {
 	} else {
 		md.inline.ruler.disable([ 'katex' ]);
 		md.block.ruler.disable([ 'katex' ]);
+	}
 	}
 }
 
@@ -1563,9 +1604,11 @@ if (localStorageGet('allow-imgur') == 'false') {
 }
 
 $$('#allow-imgur').onchange = function (e) {
+	if (!received_bye) {
 	var enabled = !!e.target.checked;
 	localStorageSet('allow-imgur', enabled);
 	allowImages = enabled;
+	}
 }
 
 if (localStorageGet('allow-old') == 'false') {
@@ -1630,24 +1673,6 @@ $$('#show-voice-btn').onchange = function (e) {
 	
 }
 
-if (localStorageGet('show-revive-btn') == 'false') {
-	$$('#show-revive-btn').checked = false;
-	showReviveBtn = false;
-	$("#revive").hide()
-}
-
-$$('#show-revive-btn').onchange = function (e) {
-	var enabled = !!e.target.checked;
-	localStorageSet('show-revive-btn', enabled);
-	showReviveBtn = enabled;
-	if (showReviveBtn) {
-		$("#revive").show()
-	} else {
-		$("#revive").hide()
-	}
-	
-}
-
 if (localStorageGet('show-voice-msg') == 'false') {
 	$$('#show-voice-msg').checked = false;
 	showVoiceMsg = false;
@@ -1681,16 +1706,6 @@ $$('#ws-cc').onchange = function (e) {
 	connectCC = enabled;
 }
 */
-
-
-/*以下为复活部分*/
-
-$("#revive").click(function () {
-	send({ cmd: 'move', channel: myChannel })
-  })
-
-/*以上为复活部分*/
-
 // User list
 var onlineUsers = [];
 var ignoredUsers = [];
@@ -1760,28 +1775,15 @@ async function getMurmur() {
         console.log(murmur)
     });
     */
-    /*
     await Fingerprint2.getPromise({}).then(components => {
-        // 参数
-        const values = components.map(function (component) {
+       // 参数
+       const values = components.map(function (component) {
             return component.value
         });
         // 指纹
         murmur = Fingerprint2.x64hash128(values[19].join(''), 31);
         //console.log(murmur)
-    })
-    */
-    await Fingerprint2.getPromise({}).then(components => {
-        // 参数
-        /*
-		const values = components.map(function (component) {
-            return component.value
-        });
-		*/
-        // 指纹
-        murmur = Fingerprint2.x64hash128(murmurseed, 31);
-        //console.log(murmur)
-    })
+   })
 }
 
 /* color scheme switcher */
@@ -1894,8 +1896,8 @@ $$('#highlight-selector').value = currentHighlight;
 
 if (myChannel == '') {
 	pushMessage({ text: homeText });
-	//$$('#footer').classList.add('hidden');
-	//$$('#sidebar').classList.add('hidden');
+	$$('#footer').classList.add('hidden');
+	$$('#sidebar').classList.add('hidden');
 } else {
 	join(myChannel);
 }
